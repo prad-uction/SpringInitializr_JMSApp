e777cbd60d9da6f2bea832a052c104b0bb90d9e0
PradeepKumarG <pradeepkumarg97.19@gmail.com>
2024-10-17 18:51:51 +0530

    Committing the JMS project

---
+++ src/main/java/com/springboot_app/java_maven_springboot/JavaMavenSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_app.java_maven_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMavenSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMavenSpringbootApplication.class, args);
+	}
+
+}
